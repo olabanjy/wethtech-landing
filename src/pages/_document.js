b596277d88074e4845ee0334bada05c6ff096e52
Chukwudi Eze <chukwudieze97@gmail.com>
2025-10-07 14:37:26 +0100

    add seo tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+        <meta
+          name="description"
+          content="Scale your business with smart finance & tax compliance technologies. Stay ahead of your finance management with smart and intuitive e-invoicing, payment and tax compliance infrastructure"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
